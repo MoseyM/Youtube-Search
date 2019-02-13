5b2a580dec2577460eb508d84e5e2af036c4b334
margaret <mmoseyee@gmail.com>
2019-02-12 21:03:32 -0600

    Created service provider for api client Added env variable for the api key

---
+++ app/Providers/GoogleAPIServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleAPIServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(\Google_Service_YouTube::class, function($app) {
+            $client = new Google_Client();
+            $client->setApplicationName(env('APP_NAME'));
+            $client->setDeveloperKey(env('YOUTUBE_API_KEY'));
+            return new \Google_Service_YouTube($client);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
